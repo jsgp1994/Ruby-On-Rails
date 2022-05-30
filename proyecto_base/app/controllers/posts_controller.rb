2e140b9a7d29cce417339657e0f25ec38504b938
Juan Sebastian Gaviria Prada <juan.gaviriap@gmail.com>
2022-05-30 04:40:35 +0000

    Se aplica la propiedad para almacenar en cache las consultas de los posts

--- proyecto_base/app/controllers/posts_controller.rb
+++ proyecto_base/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+before_action :authenticate_user!, only: [:create, :update]
+
     #rescue_from Exception do |e|
         #log.error "#{e.message}"
         #render json: {error: e.message }, status: :internal_error
@@ -15,24 +17,28 @@ class PostsController < ApplicationController
         if !params[:search].nil? && params[:search].present?
             @posts = PostsSearchService.search(@posts, params[:search])
         end
-        render json: @posts, status: :ok
+        render json: @posts.includes(:user), status: :ok
     end
 
     # GET /posts/{id}
     def show
-        @posts = Post.find(params[:id])
-        render json: @posts, status: :ok
+        @post = Post.find(params[:id])
+        if @post.published? || (Current.user && @post.user_id == Current.user.id)
+            render json: @post, status: :ok
+        else
+            render json: {error: 'Not Found'}, status: :not_found
+        end    
     end
 
     #POST /posts
-    def create
-        @post = Post.create!(create_params)
+    def create        
+        @post = Current.user.posts.create!(create_params)
         render json: @post, status: :created
     end
 
     #PUT /posts/{id}
     def update
-        @post = Post.find(params[:id])
+        @post = Current.user.posts.find(params[:id])
         @post.update!(update_params)
         render json: @post, status: :ok
     end
@@ -40,11 +46,29 @@ class PostsController < ApplicationController
     private
 
     def create_params
-        params.require(:posts).permit(:title, :content, :published, :user_id)
+        params.require(:posts).permit(:title, :content, :published)
     end
 
     def update_params
         params.require(:posts).permit(:title, :content, :published)
     end
 
+    def authenticate_user!
+        #Bearer xxxx
+        token_regex = /Bearer (\w+)/
+        #Leer HEADER de auth
+        headers = request.headers
+        #Se verifica que el token sea valido
+        if headers['Authorization'].present? && headers['Authorization'].match(token_regex)[1]
+            token = headers['Authorization'].match(token_regex)[1]
+            #debemos verificar que el token corresponda a un user
+            #truthy falsy
+            #Current funcionalidad desde rails 5 para persistir modelos
+            if (Current.user = User.find_by_auth_token(token))
+                return 
+            end
+        end
+        render json: {error: 'Unauthorized'}, status: :unauthorized
+    end 
 end
+
--- proyecto_base/app/models/user.rb
+++ proyecto_base/app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :name, presence: true
     validates :auth_token, presence: true
+
+    after_initialize :generate_auth_token
+
+    #Se invoca al generar un User.create
+    #unless es decir if !auth_token.present?
+    def generate_auth_token
+        unless auth_token.present?
+            #generar auth code
+            self.auth_token = TokenGenerationService.generate
+        end 
+    end    
+
+
 end
