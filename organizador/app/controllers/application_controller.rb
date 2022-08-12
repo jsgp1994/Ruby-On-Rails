60b2ce07e08743e780dbc90866fb92c0830225f5
Juan Gaviria <jsgp_1994@hotmail.com>
2022-08-12 22:38:59 +0000

    :books: Se carga documentación base para el paquete de autenticacion de rails

--- organizador/app/controllers/application_controller.rb
+++ organizador/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :set_local
-
+    before_action :set_local, :authenticate_user!
 
     def set_local
         I18n.locale = 'es'
